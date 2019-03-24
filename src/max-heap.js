3630b7d399f0bdc54bd83bdb35bb811b1c044246
Vyacheslav Tkachev <dragbarxd@gmail.com>
2019-03-24 23:56:36 +0300

    fix: some solve

--- src/max-heap.js
+++ src/max-heap.js
@@ -7,15 +7,23 @@ class MaxHeap {
 	}
 
 	push(data, priority) {
-		this.insertNode(data, priority);
-		this.shiftNodeUp([data, priority]);
+		let node = new Node(data, priority);
+		this.insertNode(node);
+		this.shiftNodeUp(node);
 	}
 
 	pop() {
+		if (this.isEmpty() == false) {
+			this.restoreRootFromLastInsertedNode(this.detachRoot());
+			this.shiftNodeDown(this.root);
+			return this.detachRoot().data;
+		}
 	}
 
 	detachRoot() {
-		
+		let root = this.root;
+		this.root = null;
+		return root;
 	}
 
 	restoreRootFromLastInsertedNode(detached) {
@@ -23,19 +31,31 @@ class MaxHeap {
 	}
 
 	size() {
-		
 	}
 
 	isEmpty() {
-		
+		return (this.root == null);
 	}
 
 	clear() {
-		
+		this.root = null;
+		this.parentNodes = [];
 	}
 
 	insertNode(node) {
-		// if(arguments.length === 0) node = this.root;
+		if(this.root == null){
+			this.root = node;
+			this.parentNodes.push(node);			
+		}
+		else {	
+			const firstParent = this.parentNodes[0];
+			firstParent.appendChild(node);
+			this.parentNodes.push(node);
+
+			if(firstParent.left && firstParent.right){
+				this.parentNodes.shift();
+			}
+		}
 	}
 
 	shiftNodeUp(node) {
--- src/queue.js
+++ src/queue.js
@@ -2,23 +2,35 @@ const MaxHeap = require('./max-heap.js');
 
 class PriorityQueue {
 	constructor(maxSize) {
-
+		if(maxSize == undefined )  {
+			this.maxSize = 30;
+		}
+		else {
+			this.maxSize = maxSize;
+		}
+		this.heap = new MaxHeap();
 	}
 
 	push(data, priority) {
-
+		if (this.size() >= this.maxSize) {
+			throw "error";
+		}
+		this.heap.push(data, priority);
 	}
 
 	shift() {
-
+		if (this.heap.isEmpty()) {
+			throw "error";
+		}
+		return this.heap.pop;
 	}
 
 	size() {
-
+		return this.heap.size();
 	}
 
 	isEmpty() {
-		
+		return this.heap.isEmpty();
 	}
 }
 
